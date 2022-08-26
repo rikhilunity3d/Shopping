0e5cb2c13e25f7866e11256daa149f38eecced8f
rikhilunity3d <rikhil.unity3d@gmail.com>
2022-08-26 16:21:41 +0530

    Added Editor Folder -> Assets folder -> BackgroundCreator.cs to create gameobject through Tools Menue -> CreateBackground

---
+++ Assets/Editor/BackgroundCreator.cs
@@ -0,0 +1,48 @@
+using UnityEditor;
+using UnityEngine;
+
+
+public class BackgroundCreator : EditorWindow
+{
+    string enterName = "";
+    int objectId = 1;
+    Sprite backgroundSprite;
+    GameObject objectToCreate;
+
+    [MenuItem("Tools/CreateBackground")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(BackgroundCreator));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Spaw New Object", EditorStyles.boldLabel);
+        
+        enterName = EditorGUILayout.TextField("Enter Name:",enterName);
+
+        backgroundSprite = EditorGUILayout.ObjectField("Sprite: ", backgroundSprite, typeof(Sprite)) as Sprite;
+
+        objectId = EditorGUILayout.IntField("Game Object ID", objectId);
+
+        if (GUILayout.Button("Create"))
+        {
+            CreateGameObject();
+        }
+
+        
+    }
+    private void CreateGameObject()
+    {
+        if(enterName == string.Empty)
+        {
+            EditorGUILayout.HelpBox("Error: Please enter an gameobject name to be created",MessageType.Error);
+            return;
+        }
+        objectToCreate = new GameObject{name = enterName};
+        objectToCreate.transform.position = new Vector3(0f, 0f, 0f);
+        objectToCreate.AddComponent<SetCameraOrthographicSizeAccordingToBackground>();
+        objectToCreate.GetComponent<SpriteRenderer>().sprite = backgroundSprite;
+        objectId++;
+    }
+}
